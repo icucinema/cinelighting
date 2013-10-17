8098ccb7dc61c6645115acb1a5512fffa1de735e
Luke Granger-Brown <git@lukegb.com>
2013-10-17 10:51:03 +0100

    Implements even more DMX functionality - even fading

--- lib/dmx.py
+++ lib/dmx.py
@@ -1,13 +1,134 @@
 import threading
 import time
 
+from helpers import monotonic_time
+
 DMX_MIN_VALUE = 0
 DMX_MAX_VALUE = 255
 
 DMX_MIN_CHANNEL = 1
 DMX_MAX_CHANNEL = 256
 
+
+class DmxModificationRunner(threading.Thread):
+    def __init__(self, controller, modification, interval=0.1):
+        self.controller = controller
+        self.modification = modification
+        self.interval = interval
+
+        self.calculate_values()
+
+        super(DmxModificationRunner, self).__init__()
+
+    def monotonic_clock(self):
+        return monotonic_time()
+
+    def step(self):
+        # calculate next step
+        time_now = self.monotonic_clock()
+        time_relative = min(time_now - self.started, self.duration) # cap this to the duration
+
+        step_channels = self.step_at(time_relative)
+        print time_relative, step_channels
+        self.controller.set_channels(step_channels)
+
+        return time_relative >= self.duration
+
+    def run(self):
+        self.started = self.monotonic_clock()
+        while True:
+            if self.step():
+                break
+            time.sleep(self.interval)
+
+    def calculate_values(self):
+        # calculate values
+        tv = dict(self.modification.time_values)
+        time_stops = list(sorted(tv.keys()))
+        chans = set(self.modification.using_channels)
+
+        self.time_values = tv
+        self.channels = chans
+        self.time_stops = time_stops
+        self.duration = max(time_stops)
+
+        channel_time_stops = {}
+        for ch in self.channels:
+            channel_time_stops[ch] = [time_stop for time_stop in time_stops if ch in tv[time_stop].keys()]
+        self.channel_time_stops = channel_time_stops
+
+    def calculate_easing(self, easing_type, percentage, last_val, next_val):
+        if easing_type == "linear":
+            return last_val + ((next_val - last_val) * percentage)
+        elif easing_type == "sudden":
+            # to use this easing type, you should put a stop 0.1 second before this
+            # with the LAST value
+            # then for the "FLASH" value, apply easing="sudden"
+            return next_val if percentage > 0.9 else last_val
+        raise ValueError("Unknown easing type %r" % (easing_type,))
+
+    def step_at(self, t):
+        output = {}
+        for ch in self.channels:
+            try:
+                chts = self.channel_time_stops[ch]
+
+                # find the last time that the value was specified
+                last_time = max([ts for ts in chts if ts < t])
+                last_value = self.time_values[last_time][ch]
+                # and the next time
+                next_time = min([ts for ts in chts if ts >= t])
+                next_value = self.time_values[next_time][ch]
+
+                # now what percentage of the way we are through it
+                fade_duration = float(next_time - last_time)
+                fade_progress = float(t - last_time)
+                fade_percentage = fade_progress / fade_duration
+
+                new_value = self.calculate_easing(next_value["easing"], fade_percentage, last_value['value'], next_value['value'])
+                output[ch] = int(round(new_value))
+            except ValueError:
+                pass
+                # probably means that there's no "next time" or "last time"
+                # so we should STOP FIDDLING WITH IT
+        return output
+
+
+
+
+class DmxModification(object):
+    def __init__(self, controller=None):
+        self.controller = controller  # this should ONLY be used in .execute as convenient shorthand - not required
+
+        self.locked = False
+        
+        self.using_channels = set()
+        self.time_values = {}
+
+    def execute(self):
+        """Shorthand for BaseDmxController.execute_change"""
+        assert self.controller
+        self.controller.execute_change(self)
+
+    def set_value(self, time, channel, value, easing="linear"):
+        assert not self.locked
+
+        self.using_channels.add(channel)
+        pointdict = self.time_values.setdefault(time, {}).setdefault(channel, {})
+        pointdict["value"] = value
+        pointdict["easing"] = easing
+        # space for future expansion - could stick an easing function in that dict
+
+    def lock(self):
+        assert not self.locked
+        self.locked = True
+
+
+
+
 class BaseDmxController(object):
+    """Base class describing a generic DMX controller API"""
+
     def __init__(self, starting_values=None):
         self.min_value = DMX_MIN_VALUE
         self.max_value = DMX_MAX_VALUE
@@ -64,12 +185,28 @@ class BaseDmxController(object):
 
         return self._get_channels(channel_check)
 
+    def new_change(self):
+        return DmxModification(self)
+
+    def execute_change(self, modification):
+        modification.lock()
+        modification.runner = DmxModificationRunner(self, modification)
+        modification.runner.start()
+
+
     def start(self, *args, **kwargs):
         if self.has_started:
             raise RuntimeError("Cannot 'start' multiple times")
 
         self._start(*args, **kwargs)
         self.has_started = True
+
+    def stop(self, *args, **kwargs):
+        if not self.has_started:
+            raise RuntimeError("Not started")
+
+        self._stop(*args, **kwargs)
+        self.has_started = False
 
 
     def _set_channels(self, channel_set):
@@ -79,6 +216,10 @@ class BaseDmxController(object):
         raise NotImplementedError("Subclasses should override this method and implement it")
 
     def _start(self):
+        # implement if necessary
+        pass
+
+    def _stop(self):
         # implement if necessary
         pass
 
@@ -92,6 +233,8 @@ class ManolatorDmxController(BaseDmxController):
         self.parallel = parallel
         self.parallel_keep_going = False
 
+        self.sleep_in_send = False
+
         super(ManolatorDmxController, self).__init__(*args, **kwargs)
 
     def _start(self):
@@ -101,7 +244,15 @@ class ManolatorDmxController(BaseDmxController):
             args=(self.parallel,),
             name="Manolator-Parallel-Thread"
         )
+        self.parallel_ending_event = threading.Event()
         self.parallel_update_thread.start()
+
+    def _stop(self):
+        assert self.parallel_update_thread.is_alive()
+
+        self.parallel_keep_going = False
+        if not self.parallel_ending_event.wait(5):
+            raise RuntimeError("Parallel update thread has stalled!")
 
     def _perform_parallel_update(self, p):
         # pySerial parallel in p
@@ -112,17 +263,19 @@ class ManolatorDmxController(BaseDmxController):
                 time.sleep(0.1)
                 p.setAutoFeed(0)
                 for channel in range(self.min_channel, self.max_channel):
-                    p.setData(self.live_channels[channel])
+                    val = self.live_channels[channel]
+                    p.setData(self.channel_default_value if val is None else val)
                     p.setDataStrobe(1)
-                    #time.sleep(0.01)  # Not usually necessary
+                    self.sleep_in_send and time.sleep(0.01)  # Not usually necessary
                     p.setDataStrobe(0)
-                    #time.sleep(0.01)  # Not usually necessary
+                    self.sleep_in_send and time.sleep(0.01)  # Not usually necessary
                     p.setData(0)
 
                 # now take a nap
                 time.sleep(0.1)
         finally:
             self.parallel_keep_going = False
+            self.parallel_ending_event.set()
 
 
     def _set_channels(self, channel_set):
@@ -146,8 +299,9 @@ class ManolatorDmxController(BaseDmxController):
 
         return output
 
-if __name__ == '__main__':
-    from dummyparallel import DummyParallel
-    mn = ManolatorDmxController(DummyParallel(), starting_values={76: 24})
-    mn.set_channel(72, 255)
-    mn.start()
+class DummyDmxController(BaseDmxController):
+    def _set_channels(self, channel_set):
+        print time.time(), "Setting channels:", channel_set
+
+    def _get_channels(self, channel_set):
+        return dict([(ch, 0) for ch in channel_set])
---
+++ tests/lib/dmx_test.py
@@ -0,0 +1,49 @@
+import dmx
+import dummyparallel
+
+def setup_parallel():
+    return dummyparallel.DummyParallel()
+
+def test_basic():
+    mn = dmx.ManolatorDmxController(setup_parallel())
+    mn.set_channel(72, 245)
+    mn.set_channels({
+        73: 233,
+        76: 210
+    })
+    assert mn.get_channel(72) == 245
+    assert mn.get_channel(73) == 233
+    assert mn.get_channel(76) == 210
+    assert mn.get_channels([72, 73, 76]) == {
+        72: 245,
+        73: 233,
+        76: 210
+    }
+
+def test_set_starting_values():
+    mn = dmx.ManolatorDmxController(setup_parallel(), starting_values={23: 16, 66: 11})
+    assert mn.get_channel(23) == 16
+    assert mn.get_channel(66) == 11
+
+def test_start():
+    mn = dmx.ManolatorDmxController(setup_parallel(), starting_values={23: 16, 66: 11})
+    mn.start()
+    assert mn.has_started
+    assert mn.parallel_keep_going
+    assert mn.parallel_update_thread.is_alive()
+    mn.stop()
+    assert not mn.has_started
+    assert not mn.parallel_keep_going
+    assert not mn.parallel_update_thread.is_alive()
+
+def test_fading():
+    dmdmx = dmx.DummyDmxController()
+    mod = dmdmx.new_change()
+    mod.set_value(time=0, channel=73, value=0)
+    mod.set_value(time=2, channel=74, value=255)
+    mod.set_value(time=2.1, channel=74, value=0, easing="sudden")
+    mod.set_value(time=4, channel=74, value=60)
+    mod.set_value(time=5, channel=73, value=20)
+    mod.execute()
+    mod.runner.join()
+    assert False
