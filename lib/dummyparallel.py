ee1409f81f632f0c409ae19c0664f81bd5246bf6
Luke Granger-Brown <git@lukegb.com>
2013-10-26 23:04:41 +0100

    Fix mixed spaces and tabs

--- lib/dummyparallel.py
+++ lib/dummyparallel.py
@@ -4,13 +4,13 @@ class DummyParallel(object):
     def setData(self, data):
         sys.stdout.write(str(data))
     def setAutoFeed(self, autoFeed):
-    	if autoFeed == 1:
-    		sys.stdout.write("\n")
+        if autoFeed == 1:
+            sys.stdout.write("\n")
         sys.stdout.write("[")
         sys.stdout.write(str(autoFeed))
         sys.stdout.write("]")
     def setDataStrobe(self, dataStrobe):
-    	if dataStrobe == 1:
-    		sys.stdout.write("<")
-    	else:
-    		sys.stdout.write(">")
+        if dataStrobe == 1:
+            sys.stdout.write("<")
+        else:
+            sys.stdout.write(">")
