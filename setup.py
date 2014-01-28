9b7f8f05c1c304b9a4ea5ffc642aa69b0a9fd016
Luke Granger-Brown <git@lukegb.com>
2014-01-28 23:43:02 +0000

    Move stuff around

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='CineLighting',
+    version='0.1',
+    description='Lighting utilities for Imperial Cinema',
+    author='Luke Granger-Brown',
+    author_email='git@lukegb.com',
+    url='https://github.com/icucinema/cinelighting/',
+    packages=['cinelighting']
+)
+
+    
